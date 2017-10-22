22f34c86bc1944b0304ad225251bf7e5376e2412
Jose Roque <jose.roque@mavs.uta.edu>
2017-10-21 23:45:40 -0500

    Singleton implementation v2(preferred)

--- DesignPatterns/Singleton/Elvis.java
+++ DesignPatterns/Singleton/Elvis.java
@@ -1,12 +1,7 @@
-public class Elvis implements Serializable{
-  private static final long serialVersionUID = -7604766932017737115L;
-  private Elvis() {}
-
-  public static class ElvisSerializeHelper {
-    private static final Elvis INSTANCE = new Elvis();
-  }
-
+public enum Elvis {
+  INSTANCE;
+  
   public static Elvis getInstance(){
-    return ElvisSerializeHelper.INSTANCE;
+    return INSTANCE;
   }
 }
