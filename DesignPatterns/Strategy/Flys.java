5bd4d133d12c48edecba8d7c159891d62d35907b
Jose Roque <jose.roque@mavs.uta.edu>
2017-10-23 23:54:27 -0500

    Remove extra whitespace

--- DesignPatterns/Strategy/Flys.java
+++ DesignPatterns/Strategy/Flys.java
@@ -9,32 +9,19 @@
 // I'm decoupling : encapsulating the concept that varies
 
 public interface Flys {
-
    String fly();
-
 }
 
 // Class used if the Animal can fly
-
 class ItFlys implements Flys{
-
 	public String fly() {
-
 		return "Flying High";
-
 	}
-
-
 }
 
 //Class used if the Animal can't fly
-
 class CantFly implements Flys{
-
 	public String fly() {
-
 		return "I can't fly";
-
 	}
-
 }
